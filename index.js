3c087ce4f2870b126fe841dbd34307e8e7bcdae3
Noel Manuel <setsuna04@gmail.com>
2018-02-12 05:16:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function currentLine(line) {
     var index = 1;
     while (line.length > 0) {
       sentence = sentence + ` ${index}. ${line.shift()}`
-      if (line.length) {sentence += ','}
+      if (line.length) {sentence += ',';}
       index++;
     }
   }
