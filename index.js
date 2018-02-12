7dcfcba7c9a19d7e8d0f0ac93605c679f6937c02
Noel Manuel <setsuna04@gmail.com>
2018-02-12 01:48:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ function takeANumber(katzDeliLine, name) {
 function nowServing(){
   if (katzDeliLine.length === 0) {
     return 'There is nobody waiting to be served!';
+  }
   else
     var serving = katzDeliLine.unshift();
   }
